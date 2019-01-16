9eb029babb17362e877f677f7eb3f030ba726f14
Nicholas Rodine <halfofastaple@gmail.com>
2019-01-16 18:54:27 -0500

    Added game icon, hid the place, and made it so that enemies get launched up a little bit

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,13 +5,26 @@ var THRUST = 10;
 var maxSpeed = (1000.0 / 60 / 60) * 1000;
 
 func _physics_process(delta):
-	velocity.z += THRUST * delta;
-	if (velocity.z > maxSpeed): velocity.z = maxSpeed;
+	UpdateThrust(delta);
 	UpdateInput();
+	UpdateGravity(delta);
+
+	#velocity = self.move_and_slide(velocity, Vector3(0, 1, 0), false, 100, 0.785398, false);
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		if collision.collider.has_method("hit"):
+			collision.collider.hit(velocity)
+			velocity *= 0.9;
+		else:
+			velocity = velocity.slide(collision.normal)
+
+func UpdateGravity(delta):
 	if (!is_on_floor()):
 		velocity.y -= 9.8 * delta;
-	velocity = self.move_and_slide(velocity, Vector3(0, 1, 0), false, 100, 0.785398, false);
 
+func UpdateThrust(delta):
+	velocity.z += THRUST * delta;
+	if (velocity.z > maxSpeed): velocity.z = maxSpeed;
 
 func UpdateInput():
 	var LEFT = Input.is_action_pressed("move_left");
--- Racers/Racer.gd
+++ Racers/Racer.gd
@@ -6,3 +6,6 @@ func _physics_process(delta):
 	if (!is_on_floor()):
 		velocity.y -= 9.8 * delta;
 	velocity = move_and_slide(velocity, Vector3(0, 1, 0));
+
+func hit(vel):
+	velocity = vel * 1.1 + Vector3(0, 3, 0);
