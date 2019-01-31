a1bb6fafd04d7b5b6b7f0b65eedc62e2d938673f
Nicholas Rodine <halfofastaple@gmail.com>
2019-01-30 20:19:35 -0500

    Made the player animate their drift, and made their drift accelerate. Added a 2nd light to light posts, so that it self-illuminates where the light is coming from

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,10 +3,17 @@ extends KinematicBody
 export var velocity = Vector3();
 var THRUST = 5;
 var maxSpeed = (1000.0 / 60 / 60) * 1000;
+var drift = 0;
+var DRIFT_MAX = 10;
+var DRIFT_ACCEL = 0.1;
+
+func _process(_delta):
+	$AnimationPlayer.play("yaw");
+	$AnimationPlayer.seek((-drift/2.0) + 0.5);
 
 func _physics_process(delta):
 	UpdateThrust(delta);
-	UpdateInput();
+	UpdateInput(delta);
 	UpdateGravity(delta);
 
 	var collision = move_and_collide(velocity * delta)
@@ -25,8 +32,10 @@ func UpdateThrust(delta):
 	velocity.z += THRUST * delta;
 	if (velocity.z > maxSpeed): velocity.z = maxSpeed;
 
-func UpdateInput():
+func UpdateInput(delta):
 	var LEFT = Input.is_action_pressed("move_left");
 	var RIGHT = Input.is_action_pressed("move_right");
-	velocity.x = (int(LEFT) - int(RIGHT)) * 10;
-	$VehicleMesh.rotation.z = velocity.x * .01;
+	var desire = (int(LEFT) - int(RIGHT));
+	var drift_acceleration = DRIFT_ACCEL * delta * 60;
+	drift = desire * drift_acceleration + drift * (1 - drift_acceleration);
+	velocity.x = drift * DRIFT_MAX;
